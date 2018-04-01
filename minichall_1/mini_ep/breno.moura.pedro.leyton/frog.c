175ae865b17982ae606401138f33c2a24e5bc207
--global <breno.moura@hotmail.com>
2018-04-01 20:49:18 -0300

    pushando novas paradas

---
+++ minichall_1/mini_ep/breno.moura.pedro.leyton/frog.c
@@ -0,0 +1,82 @@
+#include <pthread.h>
+#include <unistd.h>
+#include "frog.h"
+
+int valid_position(int position, int v_size) {
+    return (position >= 0 && position < v_size);
+}
+
+void printvector(int * v, int v_size) {
+    printf("\n[");
+    for (int i = 0; i < v_size; i++) {
+        if (i == 0) {
+            printf("%d", v[i]);
+        } else {
+            printf(" %d", v[i]);
+        }
+    }
+    printf("]\n");
+}
+
+void *frog_func(void *void_frog_args) {
+    fargs * frog_args = (fargs *) void_frog_args;
+    int position = frog_args->position;
+    // jump direction
+    int jd = frog_args->direction ? 1 : -1;
+    pthread_barrier_t * barrier = frog_args->barrier;
+    bool jumped;
+    int v_size = frog_args->v_size;
+    int * COUNTER = frog_args->counter;
+    int * stop_condition = frog_args->stop;
+
+    pthread_barrier_wait(barrier);
+    while (!(*stop_condition)) {
+        jumped = 0;
+
+        // position 0 or n-1 will never get here
+        // if it's moving towards the border
+        if (valid_position(position + 1 * jd, v_size) && frog_args->v[position + 1 * jd] == 0) {
+            // can jump forward
+            if (frog_args->v[position + 1 * jd] == 0) {
+                pthread_mutex_lock(frog_args->jump);
+                if (frog_args->v[position + 1 * jd] == 0) {
+                    frog_args->v[position + 1 * jd] = jd;
+                    frog_args->v[position] = 0;
+                    position = position + 1 * jd;
+                    jumped = 1;
+                }
+                pthread_mutex_unlock(frog_args->jump);
+            }
+        }
+        else {
+            // position 1 and n-2 will never get here
+            // if it's moving towards the border
+            if (valid_position(position + 2 * jd, v_size) && frog_args->v[position + 2 * jd] == 0) {
+                // can leap a friend
+                if (frog_args->v[position + 2 * jd] == 0) {
+                    pthread_mutex_lock(frog_args->jump);
+                    if (frog_args->v[position + 2 * jd] == 0) {
+                        frog_args->v[position + 2 * jd] = jd;
+                        frog_args->v[position] = 0;
+                        position = position + 2 * jd;
+                        jumped = 1;
+                    }
+                    pthread_mutex_unlock(frog_args->jump);
+                }
+            }
+        }
+
+        if (!jumped) {
+            pthread_mutex_lock(&counter_mutex);
+            (*COUNTER) = (*COUNTER) + 1;
+            pthread_mutex_unlock(&counter_mutex);
+        }
+        else {
+            pthread_mutex_lock(&counter_mutex);
+            (*COUNTER) = 0;
+            pthread_mutex_unlock(&counter_mutex);
+        }
+    }
+
+    return NULL;
+}
--- random/mini_ep.c
+++ random/mini_ep.c
@@ -1,6 +1,7 @@
 /* 
  * Demo for false sharing variables
  * Created by: Pedro Pereira, 9778794
+ * And Breno Moura, 9790973
  */
 #include <stdio.h>
 #include <pthread.h>
@@ -21,9 +22,12 @@ void *count_at(void *index_void) {
 }
 
 void sequential_count(void) {
-    while (v[0] < count_up_to && v[1] < count_up_to) {
+    while (v[0] < count_up_to && v[1] < count_up_to
+        && v[2] < count_up_to && v[3] < count_up_to) {
         v[0]++;
-	v[1]++;
+    	v[1]++;
+        v[2]++;
+        v[3]++;
     }
 }
 
@@ -36,9 +40,9 @@ void reset_v(void) {
 int main(void) {
     clock_t begin, end;
     float elapsed;
-    int a, b;
+    int a, b, c, d;
     // declaring threads
-    pthread_t ft, st;
+    pthread_t first_thread, second_thread, third_thread, fourth_thread;
 
     // sequential    
     begin = clock();
@@ -46,42 +50,59 @@ int main(void) {
     end = clock();
     elapsed = (float) (end - begin) / CLOCKS_PER_SEC;
     printf("Elapsed time in sequential: %fs\n", elapsed);
-
+    reset_v();
+    
     // with false sharing
-    a = 1;
-    b = 2;
+    a = 0;
+    b = 1;
+    c = 2;
+    d = 3;
     begin = clock();
-    if (pthread_create(&ft, NULL, count_at, &a)
-        || pthread_create(&st, NULL, count_at, &b)) {
+    if (pthread_create(&first_thread, NULL, count_at, &a)
+        || pthread_create(&second_thread, NULL, count_at, &b)
+        || pthread_create(&third_thread, NULL, count_at, &c)
+        || pthread_create(&fourth_thread, NULL, count_at, &d)) {
         
 	fprintf(stderr, "Thread creation error\n");
 	return 255;
     }
-    if (pthread_join(ft, NULL) || pthread_join(st, NULL)) {
+    if (pthread_join(first_thread, NULL) 
+        || pthread_join(second_thread, NULL)
+        || pthread_join(third_thread, NULL)
+        || pthread_join(fourth_thread, NULL)) {
         fprintf(stderr, "Error joining a thread\n");
 	return 1;
     }
     end = clock();
     elapsed = (float) (end - begin) / CLOCKS_PER_SEC;
     printf("Elapsed time with false sharing: %fs\n", elapsed);
-
+    reset_v();
+    
     // without false sharing
-    a = 1;
-    b = 999;
+    a = 0;
+    b = 250;
+    c = 500;
+    d = 999;
     begin = clock();
-    if (pthread_create(&ft, NULL, count_at, &a)
-        || pthread_create(&st, NULL, count_at, &b)) {
+    if (pthread_create(&first_thread, NULL, count_at, &a)
+        || pthread_create(&second_thread, NULL, count_at, &b)
+        || pthread_create(&third_thread, NULL, count_at, &c)
+        || pthread_create(&fourth_thread, NULL, count_at, &d)) {
         
 	fprintf(stderr, "Thread creation error\n");
 	return 255;
     }
-    if (pthread_join(ft, NULL) || pthread_join(st, NULL)) {
+    if (pthread_join(first_thread, NULL) 
+        || pthread_join(second_thread, NULL)
+        || pthread_join(third_thread, NULL)
+        || pthread_join(fourth_thread, NULL)) {
         fprintf(stderr, "Error joining a thread\n");
 	return 1;
     }
     end = clock();
     elapsed = (float) (end - begin) / CLOCKS_PER_SEC;
     printf("Elapsed time without false sharing: %fs\n", elapsed);
-
+    reset_v();
+    
     return 0;
 }
